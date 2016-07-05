ca7c4c948e827d4264b01482f910fc33c4927bca
Max Ponomarenko <maxeleron@gmail.com>
2016-07-05 16:59:20 +0300

    0.65 version
    - Added Tuple type.

---
+++ BasicTypes/Tuple.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+# Tuple creating
+a = tuple()
+
+#Creating using literal
+date = ("year", 2015)
+
+# Tuple is a list that doesn`t support changes
+data[1] = 2016 #This is eror
+
+del date[0] # This is eror too
